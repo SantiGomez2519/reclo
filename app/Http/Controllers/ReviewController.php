866d817fa0f22e0efeab5bbbbf614ac77d6a9a7b
Bosnape <pcabrejosm@gmail.com>
2025-09-02 23:58:32 -0500

    Controller | Create basic controller structure/template

---
+++ app/Http/Controllers/ReviewController.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Review;
+use Illuminate\Http\RedirectResponse;
+use Illuminate\Http\Request;
+use Illuminate\View\View;
+
+class ReviewController extends Controller
+{
+    // Here it goes the methods that are not implemented yet
+
+    // Note: Remember to validate the request in the model
+    // public function save(Request $request): View
+    // { 
+    //    Model::validate($request);
+    //    Model::create($request->only(['attribute1', 'attribute2', 'attribute3']));
+    // }
+}
