8baf736754fb7871e9db836dbebd963e8d3929f2
Bosnape <pcabrejosm@gmail.com>
2025-09-21 01:15:27 -0500

    Update | Implement PDF invoice generation with external CSS support

--- app/Util/InvoicePdfGenerator.php
+++ app/Util/InvoicePdfGenerator.php
@@ -21,15 +21,16 @@ class InvoicePdfGenerator
             'city' => 'Medellín, Colombia',
             'phone' => '+57 (4) 123-4567',
             'email' => 'info@reclo.com',
-            'website' => 'www.reclo.com'
+            'website' => 'www.reclo.com',
         ];
         $viewData['invoice_date'] = now()->format('Y-m-d');
-        $viewData['invoice_number'] = 'INV-' . str_pad($order->getId(), 6, '0', STR_PAD_LEFT);
+        $viewData['invoice_number'] = 'INV-'.str_pad($order->getId(), 6, '0', STR_PAD_LEFT);
+        $viewData['css'] = file_get_contents(public_path('css/invoice.css'));
 
         $data = ['viewData' => $viewData];
         $pdf = Pdf::loadView('invoice.pdf', $data);
-        $filename = 'invoice-' . $viewData['invoice_number'] . '.pdf';
-        
+        $filename = 'invoice-'.$viewData['invoice_number'].'.pdf';
+
         return $pdf->download($filename);
     }
 }
